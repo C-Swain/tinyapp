7642e64a8a886b33162565e388a5290e1ecf2764
C-Swain <catherinemexicoswain@gmail.com>
2021-09-27 20:26:38 +0000

    view engine Update

--- express_server.js
+++ express_server.js
@@ -4,6 +4,8 @@ const app = express();
 
 const PORT = 8080; // 8080 is the defualt port 
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
 	"b2xVn2": "http://www.lighthouselabs.ca",
 	"9sm5xK": "http://www.google.com"
@@ -12,6 +14,10 @@ const urlDatabase = {
 app.get("/", (req, res) => {
 	res.send("Hello!");
 
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get("/urls.json", (req, res) => {
